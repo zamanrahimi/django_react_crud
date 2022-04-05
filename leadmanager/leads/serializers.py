59e7187a713b2b6f00d91ff6e112104326ff2771
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-05 16:58:53 +0500

    use serializer to change model to json format

---
+++ leadmanager/leads/serializers.py
@@ -0,0 +1,11 @@
+# use serializer to change model to json format 
+from rest_framework import serializers
+from leads.models import Lead 
+
+# create serializer 
+# to change lead model to serializer 
+
+class LeadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lead 
+        field = '__all__'
